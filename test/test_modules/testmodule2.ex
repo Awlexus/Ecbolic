4d836ab75b29307355b710320725a874aaa77982
Alex <wieseralex580@gmail.com>
2018-09-09 05:26:25 +0200

    Added meta data for hexpm, moved testmodules and made sure they'll only be compiled during testing

--- test/test_modules/testmodule2.ex
+++ test/test_modules/testmodule2.ex
@@ -2,8 +2,8 @@ defmodule TestModule2 do
   use Ecbolic
 
   # This module should not be loaded outside of the test environment
-  @on_load :test_only
-  def test_only, do: if(Mix.env() == :test, do: :ok)
+  @on_load :not_prod
+  def not_prod, do: if(Mix.env() != :prod, do: :ok)
 
   @doc help: "Repeats what you just said"
   def echo(string), do: string
