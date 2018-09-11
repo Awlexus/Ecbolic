ffcf10b2ca081f29c65b60d5603cd3bc7471f14d
Alex <wieseralex580@gmail.com>
2018-09-11 20:17:33 +0200

    Replace @doc with macro calls

--- test/test_modules/testmodule.ex
+++ test/test_modules/testmodule.ex
@@ -3,17 +3,13 @@ defmodule TestModule do
 
   @moduledoc false
 
-  # This modules should not be loaded in production
-  @on_load :not_prod
-  def not_prod, do: if(Mix.env() != :prod, do: :ok)
-
-  @doc help: "returns `:world`"
+  Ecbolic.help_short "returns `:world`"
   def hello, do: :world
 
-  @doc help: "General Kenobi"
+  Ecbolic.help_short "General Kenobi"
   def hello_there, do: "General Kenobi"
 
-  @doc help: "who is that?"
+  Ecbolic.help_short "who is that?"
   def rem, do: "who is that?"
 
   def meaningless, do: ""
