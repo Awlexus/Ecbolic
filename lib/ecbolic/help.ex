62a73b9e2fea2dd524aa0f675b3d457547e75a4f
awlexus <8594605+Awlexus@users.noreply.github.com>
2018-09-17 03:19:07 +0200

    groups are now applied per function instead of per module

--- lib/ecbolic/help.ex
+++ lib/ecbolic/help.ex
@@ -1,40 +1,27 @@
 defmodule Ecbolic.Help do
-  @module_attributes [:help_group]
-  @function_attributes [:help, :help_alias]
-
   alias Ecbolic.Help
-  # [help_group: :default, help_alias: nil, help: nil]
+    
   defstruct help_group: :default, help: nil, help_alias: nil
-  @type t :: %Help{help_group: atom, help: atom, help: String.t()}
+  @type t :: %Help{help_group: atom, help: atom, help_alias: String.t()}
 
+  @doc """
+  Goes through the documentation of a module, parses it's meta
+  data and returns returns the a Help struct for each function 
+  with matching attributes
+
+  See `Ecbolic` information on how to add helping meta data to
+  a function
+  """
+  @spec to_help_entries(atom) :: [Help.t()]
   def to_help_entries(module) do
-    with {:docs_v1, _anno, :elixir, _format, _module_doc, meta, docs} <- Code.fetch_docs(module) do
-      mattributes = module_attributes(meta)
-      fattributes = function_attributes(docs)
+    {:docs_v1, _anno, :elixir, _format, _module_doc, _meta, docs} = Code.fetch_docs(module)
 
-      base_entry = Map.merge(%Help{}, mattributes)
-
-      Enum.map(fattributes, fn fattribute ->
-        Map.merge(base_entry, fattribute)
-      end)
-    end
-  end
-
-  defp module_attributes(:none), do: %{}
-  defp module_attributes(:hidden), do: %{}
-
-  defp module_attributes(doc) do
-    doc
-    |> Map.take(@module_attributes)
-  end
-
-  defp function_attributes(docs) do
     docs
-    |> Enum.reduce([], &function/2)
-    |> Enum.map(&Map.take(&1, @function_attributes))
+    |> Enum.reduce([], &meta_data/2)
+    |> Enum.map(&struct(Help, &1))
   end
 
-  defp function(doc, acc) do
+  defp meta_data(doc, acc) do
     case doc do
       {_, _, _, _, %{help: _, help_alias: _} = meta} -> [meta | acc]
       {{_, name, _}, _, _, _, %{help: _} = meta} -> [Map.merge(meta, %{help_alias: name}) | acc]
